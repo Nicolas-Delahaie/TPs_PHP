30f3660d404bc8360d435db6b90c9fbf4fa773d7
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2022-11-30 12:03:29 +0100

    Init fichiers

---
+++ TP7/login.php
@@ -0,0 +1,12 @@
+<?php
+    echo $pseudo;
+    echo $pwd;
+    // session_start();
+    // session_register();
+    // session_unregister();
+    // session_is_registered();
+    // session_id();    
+    // session_name();
+    // session_unset();
+    // session_destroy();
+?>
